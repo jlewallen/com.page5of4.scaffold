e719ce67119f4817e772e42bb201397b0f366b48
Jacob Lewallen <jlewallen@gmail.com>
2011-07-01 21:05:20 -0700

    Enabling database/JPA in sample.

---
+++ com.page5of4.scaffold.sample1/src/main/java/com/page5of4/scaffold/sample1/server/SimpleServer.java
@@ -0,0 +1,15 @@
+package com.page5of4.scaffold.sample1.server;
+
+public class SimpleServer {
+
+   /*
+    HttpHandler handler = ContainerFactory.createContainer(
+      HttpHandler.class,
+      HelloWorldResource.class);
+
+    HttpServer server = HttpServer.create(new InetSocketAddress(9998), 0);
+    server.createContext("/", handler);
+    server.setExecutor(null);
+    server.start();
+    */
+}
