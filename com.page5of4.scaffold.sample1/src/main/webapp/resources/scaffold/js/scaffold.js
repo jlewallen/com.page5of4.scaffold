3285cacbb5f4398a9f02de29dc6914657be94db7
Jacob Lewallen <jlewallen@gmail.com>
2011-07-04 10:30:22 -0700

    Cleaning up some JS.

--- com.page5of4.scaffold.sample1/src/main/webapp/resources/scaffold/js/scaffold.js
+++ com.page5of4.scaffold.sample1/src/main/webapp/resources/scaffold/js/scaffold.js
@@ -1,5 +1,8 @@
 (function($) {
    $(function() {
       $('.scaffold.editor .date-field').datepicker();
+      $('.scaffold.editor .date-time-field').datetimepicker({
+
+      });
    });
 })(jQuery);
