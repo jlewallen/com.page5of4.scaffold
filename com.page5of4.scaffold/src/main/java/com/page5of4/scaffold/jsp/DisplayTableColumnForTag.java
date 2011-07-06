5d35879b0ef9f8e976cff9a9487af28452724568
Jacob Lewallen <jlewallen@gmail.com>
2011-07-06 16:18:34 -0700

    Truncating in tables. Can override table templates now...

---
+++ com.page5of4.scaffold/src/main/java/com/page5of4/scaffold/jsp/DisplayTableColumnForTag.java
@@ -0,0 +1,12 @@
+package com.page5of4.scaffold.jsp;
+
+public class DisplayTableColumnForTag extends ScaffoldForTag {
+
+   private static final long serialVersionUID = 1L;
+
+   @Override
+   public String getMode() {
+      return "table";
+   }
+
+}
