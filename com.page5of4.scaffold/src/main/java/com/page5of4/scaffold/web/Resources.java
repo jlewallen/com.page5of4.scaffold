d5c7df68d273d9254662a6a1859fd9172f076f27
Jacob Lewallen <jlewallen@gmail.com>
2011-07-05 22:45:00 -0700

    Pagination on the view.

--- com.page5of4.scaffold/src/main/java/com/page5of4/scaffold/web/Resources.java
+++ com.page5of4.scaffold/src/main/java/com/page5of4/scaffold/web/Resources.java
@@ -1,6 +1,8 @@
 package com.page5of4.scaffold.web;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Resources<T extends Object> {
    private final Collection<T> resources;
@@ -24,11 +26,52 @@ public class Resources<T extends Object> {
       return page;
    }
 
+   public List<Page> getPages() {
+      List<Page> pages = new ArrayList<Page>();
+      for(int i = 0; i < numberOfPages; ++i) {
+         pages.add(new Page(i, page == i));
+      }
+      return pages;
+   }
+
+   public Page getPreviousPage() {
+      if(page <= 0) return null;
+      return new Page(page - 1, false);
+   }
+
+   public Page getNextPage() {
+      if(page >= numberOfPages - 1) return null;
+      return new Page(page + 1, false);
+   }
+
    public Resources(Class<T> resourceClass, Collection<T> resources, int numberOfPages, int page) {
       super();
       this.resourceClass = resourceClass;
       this.resources = resources;
       this.numberOfPages = numberOfPages;
       this.page = page;
+   }
+
+   public static class Page {
+      private int number;
+      private boolean selected;
+
+      public int getNumber() {
+         return number;
+      }
+
+      public boolean isSelected() {
+         return selected;
+      }
+
+      public String getUrl() {
+         return "?page=" + number;
+      }
+
+      public Page(int number, boolean selected) {
+         super();
+         this.number = number;
+         this.selected = selected;
+      }
    }
 }
