ddde924f21fc37e86a8c5c123a450e2c4cf87781
Jacob Lewallen <jlewallen@gmail.com>
2011-08-15 23:28:21 -0700

    In testing, Card will be our custom controller...

--- com.page5of4.scaffold.sample1/src/main/java/com/page5of4/scaffold/sample1/web/CardController.java
+++ com.page5of4.scaffold.sample1/src/main/java/com/page5of4/scaffold/sample1/web/CardController.java
@@ -3,8 +3,10 @@ package com.page5of4.scaffold.sample1.web;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.page5of4.scaffold.RepositoryAwarePropertyEditor;
 import com.page5of4.scaffold.domain.Repository;
@@ -12,8 +14,8 @@ import com.page5of4.scaffold.metadata.ScaffoldViewModelFactory;
 import com.page5of4.scaffold.metadata.TemplateMetadataFactory;
 import com.page5of4.scaffold.web.GenericScaffoldController;
 
-// @Controller
-// @RequestMapping(value = "/admin/cards")
+@Controller
+@RequestMapping(value = "/admin/cards")
 public class CardController extends GenericScaffoldController<String, Card> {
 
    @Autowired
--- com.page5of4.scaffold/src/main/java/com/page5of4/scaffold/web/DefaultScaffoldController.java
+++ com.page5of4.scaffold/src/main/java/com/page5of4/scaffold/web/DefaultScaffoldController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.HandlerMapping;
 
@@ -57,6 +58,7 @@ public class DefaultScaffoldController extends AbstractScaffoldController {
    }
 
    @Override
+   @InitBinder
    public void initializeBinder(WebDataBinder binder) {
       for(Class<?> klass : configurer.findAllScaffoldClasses()) {
          binder.registerCustomEditor(klass, new RepositoryAwarePropertyEditor(getRepository(), klass));
